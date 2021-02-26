8dece9c571b072762f26e8d919448bfe312dd1cb
albilu <>
2021-02-26 16:09:49 +0100

    Upgrade Jsoup to 1.13.1 Add Java Unirest for Http calls (Replace Jsoup Http call by Unirest http call) Fix stackoverflow: use of ST API as ST is blocking now scrappers Fix dockerhub: use of docker API as scrapping is not working anymore Fix Github, Google, Npm selector Add secure urls

--- src/org/chrisle/netbeans/plugins/nbquicksearchextender/searchprovider/DockerHubSearchProvider.java
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/searchprovider/DockerHubSearchProvider.java
@@ -5,13 +5,15 @@ import org.netbeans.spi.quicksearch.SearchRequest;
 import org.netbeans.spi.quicksearch.SearchResponse;
 
 public class DockerHubSearchProvider extends QuickSearchProvider {
-    public DockerHubSearchProvider() {
-        super.setSearchUrl("https://hub.docker.com/search/?isOfficial=0&q=");
-        super.setSelector(".profile-repos ul li a");
-    }
 
-    @Override
-    public void evaluate(SearchRequest request, SearchResponse response) {
-        super.evaluate(request, response);
-    }
+	public DockerHubSearchProvider() {
+		super.setBaseUrl("https://hub.docker.com/r/");
+		super.setSearchUrl("https://hub.docker.com/api/content/v1/products/search/?source=community&type=image&q=");
+	}
+
+	@Override
+	public void evaluate(SearchRequest request, SearchResponse response) {
+		super.evaluate(request, response);
+	}
+
 }
--- src/org/chrisle/netbeans/plugins/nbquicksearchextender/searchprovider/StackoverflowSearchProvider.java
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/searchprovider/StackoverflowSearchProvider.java
@@ -4,16 +4,14 @@ import org.chrisle.netbeans.plugins.nbquicksearchextender.QuickSearchProvider;
 import org.netbeans.spi.quicksearch.SearchRequest;
 import org.netbeans.spi.quicksearch.SearchResponse;
 
-
 public class StackoverflowSearchProvider extends QuickSearchProvider {
 
-    public StackoverflowSearchProvider() {
-        super.setSearchUrl("http://stackoverflow.com/search?q=");
-        super.setSelector(".summary>.result-link a");
-    }
+	public StackoverflowSearchProvider() {
+		super.setSearchUrl("https://api.stackexchange.com/2.2/search?sort=activity&site=stackoverflow&intitle=");
+	}
 
-    @Override
-    public void evaluate(SearchRequest request, SearchResponse response) {
-        super.evaluate(request, response); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public void evaluate(SearchRequest request, SearchResponse response) {
+		super.evaluate(request, response); //To change body of generated methods, choose Tools | Templates.
+	}
 }
